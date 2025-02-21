3ed79993ed7d674e214508f58b22cca3ff4d9c7d
elissandroa <elissandroa@hotmail.com>
2025-02-21 20:41:54 -0300

    Start Project

---
+++ backend/src/test/java/br/com/elissandro/scoolrollcall/ScoolrollcallApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elissandro.scoolrollcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoolrollcallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
